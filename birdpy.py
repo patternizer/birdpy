645bdcc01a0450dee997aff2e19eea5ae4b4f32d
patternizer <patternizer@gmail.com>
2020-01-21 16:59:00 +0000

    Code tidy up

--- birdpy.py
+++ birdpy.py
@@ -2,8 +2,8 @@
 
 # PROGRAM: birdpy.py
 #-----------------------------------------------------------------------
-# Version 0.1
-# 18 January, 2020
+# Version 0.2
+# 21 January, 2020
 # patternizer AT gmail DOT com
 # https://patternizer.github.io
 #-----------------------------------------------------------------------
@@ -18,9 +18,9 @@ from moviepy.config import change_settings
 change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.0.9-Q16\\magick.exe"})
 
 #-----------------------------------------------------------------------------------------------
-# Animated matplotlib with audio segment
-duration = 37
-audioclip = AudioFileClip("ambient.mp3").set_end(duration)
+# Total duration and audio segment
+duration = 58.7
+audioclip = AudioFileClip("soundtrack.mp3").set_end(duration)
 
 # Load the raw film and select the subclip 00:00:11 - 00:00:16
 clip = (VideoFileClip("film.MOV", audio=False)
@@ -30,12 +30,44 @@ clip = (VideoFileClip("film.MOV", audio=False)
         .fx( vfx.speedx, 1) # single speed
         .fx( vfx.colorx, 1.0)) # darken the picture
 
+# Loop #1: repeat speed
+loop1 = (VideoFileClip("film.MOV", audio=False)
+        .volumex(0.1) 
+        .subclip(11.5,15) 
+        .fx( vfx.resize, width=800) 
+        .fx( vfx.speedx, 1.0) 
+        .fx( vfx.colorx, 1.0))
+
+# Loop #2: 75% speed
+loop1 = (VideoFileClip("film.MOV", audio=False)
+        .volumex(0.1) 
+        .subclip(11.5,15) 
+        .fx( vfx.resize, width=800) 
+        .fx( vfx.speedx, 0.75) 
+        .fx( vfx.colorx, 1.0))
+
+# Loop #3: 50% speed
+loop2 = (VideoFileClip("film.MOV", audio=False)
+        .volumex(0.1) 
+        .subclip(11.5,15) 
+        .fx( vfx.resize, width=800) 
+        .fx( vfx.speedx, 0.5) 
+        .fx( vfx.colorx, 1.0)) 
+
+# Loop #4: 25% speed
+loop3 = (VideoFileClip("film.MOV", audio=False)
+        .volumex(0.1) 
+        .subclip(11.5,13.5) 
+        .fx( vfx.resize, width=800) 
+        .fx( vfx.speedx, 0.25) 
+        .fx( vfx.colorx, 1.0))
+
 # Get clip dimensions
 w,h = clip.size
 
 # Save start and end frames
 clip.save_frame("frame_start.png", t=0) # saves the frame a t=0s
-clip.save_frame("frame_end.png", t=clip.duration) # saves the last frame
+clip.save_frame("frame_end.png", t=loop3.duration-0.2) # saves the last frame
 
 # Make title and author clip
 first_frame = ImageClip('frame_start.png').set_duration(3)
@@ -44,13 +76,11 @@ clip_the_title = the_title.set_pos('center').set_duration(3)
 title = CompositeVideoClip([first_frame, clip_the_title]) # Overlay text on image
 
 first_frame = ImageClip('frame_start.png').set_duration(3)
-the_author = TextClip("Michael Taylor \n ∫∫∫∫∫∫∫∫∫∫∫∫∫∫∫∫∫∫ \n patternizer@gmail.com", fontsize=50, color='Black')
+the_author = TextClip("Michael Taylor\n patternizer@gmail.com", fontsize=50, color='Black')
 clip_the_author = the_author.set_pos('center').set_duration(3)
 author = CompositeVideoClip([first_frame, clip_the_author]) # Overlay text on image
 
 # Make the end clip
-# The mask is a circle with vanishing radius r(t) = 800-200*t               
-#last_frame.mask.get_frame = lambda t: circle(screensize=(w,h), center=(w/2,h/4), radius=max(0,int(800-200*t)), col1=1, col2=0, blur=4)
 last_frame = ImageClip('frame_end.png').set_duration(3)
 the_end = TextClip("The End", fontsize=50, color="black")
 clip_the_end = the_end.set_pos('center').set_duration(3)
@@ -64,10 +94,9 @@ clip_the_credits = the_credits.set_pos(lambda t:('center',-5*t)).set_duration(5)
 credits = CompositeVideoClip([last_frame, clip_the_credits])
 
 # Join up the clips
-final = concatenate_videoclips([title, author, clip, end, credits]).set_audio(audioclip)
+final = concatenate_videoclips([title, author, clip, loop1, loop2, loop3, end, credits]).set_audio(audioclip)
 
 # Write to file
-#video.preview(fps=30)
 final.write_videofile('ornithography_1.mp4',fps=25)
 #-----------------------------------------------------------------------------------------------
 
