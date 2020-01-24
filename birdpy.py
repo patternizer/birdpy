aa4b19d075318a1cffb6cf0917c4cdd017cb0640
patternizer <patternizer@gmail.com>
2020-01-24 00:44:18 +0000

    Added motion sensing and tracking algorithm

--- birdpy.py
+++ birdpy.py
@@ -2,12 +2,13 @@
 
 # PROGRAM: birdpy.py
 #-----------------------------------------------------------------------
-# Version 0.2
-# 21 January, 2020
+# Version 0.3
+# 23 January, 2020
 # patternizer AT gmail DOT com
 # https://patternizer.github.io
 #-----------------------------------------------------------------------
 
+import cv2
 from moviepy.editor import *
 from moviepy.video.io.bindings import mplfig_to_npimage
 from moviepy.video.tools.segmenting import findObjects
@@ -27,8 +28,11 @@ clip = (VideoFileClip("film.MOV", audio=False)
         .volumex(0.1) # quieten audio
         .subclip(6,16) # subclip: 00:00:11 - 00:00:16
         .fx( vfx.resize, width=800) # resize (keep aspect ratio)
-        .fx( vfx.speedx, 1) # single speed
+        .fx( vfx.speedx, 1.0) # single speed
         .fx( vfx.colorx, 1.0)) # darken the picture
+
+# Get clip dimensions
+clip_w, clip_h = clip.size
 
 # Loop #1: repeat speed
 loop1 = (VideoFileClip("film.MOV", audio=False)
@@ -39,7 +43,7 @@ loop1 = (VideoFileClip("film.MOV", audio=False)
         .fx( vfx.colorx, 1.0))
 
 # Loop #2: 75% speed
-loop1 = (VideoFileClip("film.MOV", audio=False)
+loop2 = (VideoFileClip("film.MOV", audio=False)
         .volumex(0.1) 
         .subclip(11.5,15) 
         .fx( vfx.resize, width=800) 
@@ -47,7 +51,7 @@ loop1 = (VideoFileClip("film.MOV", audio=False)
         .fx( vfx.colorx, 1.0))
 
 # Loop #3: 50% speed
-loop2 = (VideoFileClip("film.MOV", audio=False)
+loop3 = (VideoFileClip("film.MOV", audio=False)
         .volumex(0.1) 
         .subclip(11.5,15) 
         .fx( vfx.resize, width=800) 
@@ -55,19 +59,70 @@ loop2 = (VideoFileClip("film.MOV", audio=False)
         .fx( vfx.colorx, 1.0)) 
 
 # Loop #4: 25% speed
-loop3 = (VideoFileClip("film.MOV", audio=False)
+loop4 = (VideoFileClip("film.MOV", audio=False)
         .volumex(0.1) 
         .subclip(11.5,13.5) 
         .fx( vfx.resize, width=800) 
         .fx( vfx.speedx, 0.25) 
         .fx( vfx.colorx, 1.0))
 
-# Get clip dimensions
-w,h = clip.size
+loop1.write_videofile('clip_untracked.avi', fps=25, codec='libx264')
+
+#--------------------
+# AI Motion Detection
+#--------------------
+
+# Open AVI for AI capture
+cap = cv2.VideoCapture('clip_untracked.avi')
+frame_width = int( cap.get( cv2.CAP_PROP_FRAME_WIDTH) )
+frame_height = int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT) )
+
+# Begin AI motion tracking
+fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+out = cv2.VideoWriter("clip_tracked.avi", fourcc, 25.0, (clip_w,clip_h))
+ret, frame1 = cap.read()
+ret, frame2 = cap.read()
+
+while cap.isOpened():
+
+    try:
+        diff = cv2.absdiff(frame1, frame2)
+        gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (5,5), 0)
+        _, thresh = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY)
+        dilated = cv2.dilate(thresh, None, iterations=3)
+        contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            (x, y, w, h) = cv2.boundingRect(contour)
+
+            if cv2.contourArea(contour) > 500:
+                continue
+            cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+            #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+
+        image = cv2.resize(frame1, (clip_w,clip_h))
+        out.write(image)
+        cv2.imshow("feed", frame1)
+        frame1 = frame2
+        ret, frame2 = cap.read()
+
+        if cv2.waitKey(40) == 27:
+            break
+
+    except:
+        pass
+
+cv2.destroyAllWindows()
+cap.release()
+out.release()
+
+clip_tracked = VideoFileClip("clip.tracked.avi", audio=False)
 
 # Save start and end frames
 clip.save_frame("frame_start.png", t=0) # saves the frame a t=0s
-clip.save_frame("frame_end.png", t=loop3.duration-0.2) # saves the last frame
+clip_tracked.save_frame("frame_end.png", t=clip_tracked.duration-0.2) # saves the last frame
 
 # Make title and author clip
 first_frame = ImageClip('frame_start.png').set_duration(3)
@@ -94,7 +149,7 @@ clip_the_credits = the_credits.set_pos(lambda t:('center',-5*t)).set_duration(5)
 credits = CompositeVideoClip([last_frame, clip_the_credits])
 
 # Join up the clips
-final = concatenate_videoclips([title, author, clip, loop1, loop2, loop3, end, credits]).set_audio(audioclip)
+final = concatenate_videoclips([title, author, clip, loop1, loop2, loop3, loop4, clip_tracked, end, credits]).set_audio(audioclip)
 
 # Write to file
 final.write_videofile('ornithography_1.mp4',fps=25)
